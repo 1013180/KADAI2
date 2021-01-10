c0e666e7bac0677d78c07524bf7c77a5b24f394c
1013180 <hiroki.kojima0417@gmail.com>
2021-01-10 13:09:59 +0000

    [Update]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
+    def update
+     book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(blog)
+  end
+
+    private
+  def book_params
+    params.require(:book).permit(:title, :body)
+end
+end
