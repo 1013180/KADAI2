b3a638546ab2d73f9437ade78d049c131c2a26e2
1013180 <hiroki.kojima0417@gmail.com>
2021-01-11 13:07:58 +0000

    [Update2]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,26 @@ class BooksController < ApplicationController
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def index
     @book = Book.new
     @books = Book.all
-    logger.debug(@books.to_yaml)
   end
 
   def top
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :index
+  end
   end
 
   def edit
@@ -26,19 +31,21 @@ class BooksController < ApplicationController
   end
 
     def update
-     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(blog)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book)
+    else
+    render("books/new")
+    end
     end
 
    def destroy
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
    end
 
 
-    private
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
