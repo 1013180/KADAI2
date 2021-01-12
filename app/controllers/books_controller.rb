0a7843e7fd1d2093d0a33237c37712c985f187f9
1013180 <hiroki.kojima0417@gmail.com>
2021-01-12 08:13:05 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
     @books = Book.all
      @book = Book.new(book_params)
     if @book.save
-    flash[:notice] = "Book was successfully updated."
+    flash[:notice] = "Book was successfully createed."
     redirect_to book_path(@book)
     else
     render :index
@@ -33,9 +33,10 @@ class BooksController < ApplicationController
     def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book)
     else
-    render("books/new")
+    render :edit
     end
     end
 
